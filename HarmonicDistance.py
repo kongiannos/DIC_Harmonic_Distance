e87921e3f19a51f5fb24ec3b04d12bb4911725df
KonGiannos <kongiannos@gmail.com>
2019-02-20 16:42:09 +0200

    error corrections; added plot function

--- HarmonicDistance.py
+++ HarmonicDistance.py
@@ -6,24 +6,55 @@ Created on Tue Feb 12 11:15:44 2019
 """
 
 #Import packages
-from music21 import *
+import music21 as m21
 import numpy as np
 import computeDIC as dic
-import reduction
+#For plotting correlation coeffiecient vector
+import matplotlib.pyplot as plt
 
-#Read 2 xml files
-f1 = reduction.chordifyReduction('file1.xml')
-f2 = reduction.chordifyReduction('file2.xml')
-
-#Compute a list of DICs for each file
-DIC1 = dic.computeDICsfromChordList(f1)
-DIC2 = dic.computeDICsfromChordList(f2)
-
-#Calculate the respective correlation coefficients (Pearson's product)
-coef = []
-if len(DIC1) != len(DIC2):
-    for i in len(DIC1):
-        coef[i] = np.corrcoef(DIC1[i],DIC2[i])[0][1]
-else:
-    print('The harmonisations do not have the same amount of chords')
+def dicDistanceFromFiles(fileName1,fileName2):
+    #Read 2 xml files
+    # parse piece
+    p = m21.converter.parse(fileName1)
+    # reduction
+    r1 = p.parts[-1]
+    r2 = p.parts[-2]
+    rc = m21.stream.Score()
+    rc.insert(0, r1)
+    rc.insert(0, r2)
+    rcChordified = rc.chordify()
+    rcFlat = rcChordified.flat
+    reduction = rcFlat.getElementsByClass('Chord')
+    chordsAll = []
+    for ch in reduction:
+        chord = [c.pitch.midi%12 for c in ch]
+        chordsAll.append(chord)
+    f1 = chordsAll
     
+    p = m21.converter.parse(fileName2)
+    # reduction
+    r1 = p.parts[-1]
+    r2 = p.parts[-2]
+    rc = m21.stream.Score()
+    rc.insert(0, r1)
+    rc.insert(0, r2)
+    rcChordified = rc.chordify()
+    rcFlat = rcChordified.flat
+    reduction = rcFlat.getElementsByClass('Chord')
+    chordsAll = []
+    for ch in reduction:
+        chord = [c.pitch.midi%12 for c in ch]
+        chordsAll.append(chord)
+    f2 = chordsAll
+    
+    #Compute a list of DICs for each file
+    dic1 = dic.computeDICsfromChordList(f1)
+    dic2 = dic.computeDICsfromChordList(f2)
+    
+    #Calculate the respective correlation coefficients (Pearson's product)
+    coef = np.zeros(dic1.shape[0])
+    for i in range(dic1.shape[0]):
+        coef[i] = np.corrcoef(dic1[i,:],dic2[i,:])[0][1]
+        
+    plt.plot(coef)
+    return coef
